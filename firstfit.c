4c7a9930965ae9f906ace701e885af3f0fc96ff0
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2024-07-08 17:19:12 +0530

    Update firstfit.c

--- firstfit.c
+++ firstfit.c
@@ -1,42 +1,46 @@
-#include<stdio.h>
-void main()
-   {
-    int Process[5];
-    
-    struct memory{    //Memory available
-       int size;
-       int flag;
-     }m[5];
-     
-    printf("Enter the memory used by the process\n");
-    for (int i=0;i<5;i++)
-        scanf("%d",&Process[i]);
-        
-    printf("Enter the size of memory slots available :\n");
-    for (int i=0;i<5;i++)
-       {
-        scanf("%d",&m[i].size);
-        m[i].flag=0;
-       }
-       
-    //Memory Allocation
-    for (int i=0;i<5;i++)
-       {
-        for (int j=0;j<5;j++)
-           {
-            if(Process[i]<=m[j].size)
-               {
-                if (m[j].flag==1)
-                   continue;
-                else
-                   {
-                    printf("Process %d is allocated to %d Memory\n",i+1,m[j].size);
-                    m[j].flag=1;
-                    break;
-                   }
-               }
-          
-               
-           }
-       }   
-   }
+#include <stdio.h>
+
+void main() {
+    int memblok, limit, i, j;
+    printf("Enter the number of memory blocks and number of processes\n");
+    scanf("%d%d", &memblok, &limit);
+
+    struct memory {
+        int size, alloc;
+    } m[memblok];
+
+    struct process {
+        int psize, flag;
+    } p[limit];
+
+    printf("Enter the size of memory blocks\n");
+    for (i = 0; i < memblok; i++) {
+        scanf("%d", &m[i].size);
+        m[i].alloc = 0;
+    }
+
+    printf("Enter the size of processes\n");
+    for (i = 0; i < limit; i++) {
+        scanf("%d", &p[i].psize);
+        p[i].flag = 0;
+    }
+
+    // First Fit Allocation
+    for (i = 0; i < limit; i++) {
+        for (j = 0; j < memblok; j++) {
+            if (p[i].flag == 0 && p[i].psize <= m[j].size && m[j].alloc == 0) {
+                m[j].alloc = 1;
+                p[i].flag = 1;
+                printf("Process %d is allocated in %d block\n", p[i].psize, m[j].size);
+                break; // Move to the next process after allocation
+            }
+        }
+    }
+
+    for (i = 0; i < limit; i++) {
+        if (p[i].flag == 0) {
+            printf("There is no space for %d process\n", p[i].psize);
+        }
+    }
+}
+
